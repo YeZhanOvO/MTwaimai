7498cbb68450aa0c695ece65ca01aa3ea4e5bd1b
少林功夫好 <982282139@qq.com>
2020-11-20 17:31:55 +0800

    新项目

---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		主页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
